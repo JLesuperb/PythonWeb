56971cecf9a47de0bb4656d5c4e0c00c6a6bcd91
JLesuperb <lesuperbjonathan@gmail.com>
2016-03-19 16:10:39 -0700

    Added files via upload

---
+++ framework-mvc/models/users.py
@@ -0,0 +1,8 @@
+class Users:
+    name = {"firstName": "Jonathan", "lastName": "Lesuperb"}
+
+    def __init__(self):
+        self.name = {"firstName": "Jonathan", "lastName": "Lesuperb"}
+
+    def getNames(self):
+        return self.name
